fd867faf74ec622d359f10277f4b3980f7cb05db
MaksymZelisko <maksym.zelisko.pz.2021@lpnu.ua>
2023-12-06 00:18:22 +0200

    Added new tests for right html creation

--- site/src/app/home/home.component.spec.ts
+++ site/src/app/home/home.component.spec.ts
@@ -1,23 +1,30 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+// home.component.spec.ts
 
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HomeComponent } from './home.component';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [HomeComponent]
-    })
-    .compileComponents();
-    
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HomeComponent],
+    });
     fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create the component', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have h1 with text "DreamTeam" inside a div', () => {
+    const divElement: HTMLDivElement | null = fixture.nativeElement.querySelector('div');
+    expect(divElement).toBeTruthy();
+
+    expect(divElement?.querySelector('h1')).toBeTruthy();
+    expect((divElement?.querySelector('h1'))?.textContent).toContain('DreamTeam');
   });
 });
